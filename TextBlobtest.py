abec6818a357a406e0d3a4b6f0bff83a021190d3
theang66 <anguyen1@macalester.edu>
2020-04-23 16:30:58 -0500

    Initial TextBlob code

---
+++ TextBlobtest.py
@@ -0,0 +1,17 @@
+from textblob import TextBlob
+import nltk
+# import ssl
+#
+# try:
+#     _create_unverified_https_context = ssl._create_unverified_context
+# except AttributeError:
+#     pass
+# else:
+#     ssl._create_default_https_context = _create_unverified_https_context
+#
+# Need to install brown, punkt, wordnet, averaged_perceptron_tagger, conll2000, movie_
+# nltk.download()
+
+input = input("Talk to me! ")
+wiki = TextBlob(input)
+print("Polarity:", wiki.sentiment.polarity)
