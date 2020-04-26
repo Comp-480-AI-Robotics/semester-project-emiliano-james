6ab582b778b356f91777ff997bb712869503db2b
theang66 <anguyen1@macalester.edu>
2020-04-26 15:35:05 -0500

    Added intervals for emotions

--- TextBlobtest.py
+++ TextBlobtest.py
@@ -12,6 +12,26 @@ import nltk
 # Need to install brown, punkt, wordnet, averaged_perceptron_tagger, conll2000, movie_reviews
 # nltk.download()
 
-input = input("Talk to me! ")
-wiki = TextBlob(input)
-print("Polarity:", wiki.sentiment.polarity, "Subjectivity:", wiki.sentiment.subjectivity)
+while True:
+    txt = input("Talk to me! (Type 'q' to quit) ")
+    wiki = TextBlob(txt)
+
+    if wiki.lower() == "q":
+        break
+
+    emotion = ""
+    polarity = wiki.sentiment.polarity
+    subjectivity = wiki.sentiment.subjectivity
+
+    if -0.2 <= polarity <= 0.2:
+        emotion = "Neutral"
+    elif 0.2 < polarity <= 0.6:
+        emotion = "Somewhat positive"
+    elif 0.6 < polarity <= 1.0:
+        emotion = "Very positive"
+    elif -0.6 <= polarity < -0.2:
+        emotion = "Somewhat negative"
+    elif -1 <= polarity < -0.6:
+        emotion = "Very negative"
+
+    print("Polarity:", polarity, "Subjectivity:", subjectivity, "Emotion:", emotion, "\n")
