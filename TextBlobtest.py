adcb59c203829b678fe0673c882eb6261640dce2
theang66 <anguyen1@macalester.edu>
2020-04-26 14:44:14 -0500

    Minor changes

--- TextBlobtest.py
+++ TextBlobtest.py
@@ -9,9 +9,9 @@ import nltk
 # else:
 #     ssl._create_default_https_context = _create_unverified_https_context
 #
-# Need to install brown, punkt, wordnet, averaged_perceptron_tagger, conll2000, movie_
+# Need to install brown, punkt, wordnet, averaged_perceptron_tagger, conll2000, movie_reviews
 # nltk.download()
 
 input = input("Talk to me! ")
 wiki = TextBlob(input)
-print("Polarity:", wiki.sentiment.polarity)
+print("Polarity:", wiki.sentiment.polarity, "Subjectivity:", wiki.sentiment.subjectivity)
