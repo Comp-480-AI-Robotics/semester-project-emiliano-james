af85bda95717131341365e8c79c54e03f4bdbbbb
Lily <lily.irvin@gmail.com>
2020-04-28 14:22:36 -0500

    identify eye contours

--- kuriEmotions.py
+++ kuriEmotions.py
@@ -12,38 +12,26 @@ dim = (width, height)
 img = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
 # draw circles around eyes
-# newImg, contours, hier = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-# for cnt in contours:
-#     cntArea = cv2.contourArea(cnt)
-#     if cnt.Area > 1000:
-#         ellipse = cv2.fitEllipse(cnt)
+greyImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+res, thresh = cv2.threshold(greyImg, 120, 255, 0)
+newImg, contours, hier = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+for cnt in contours:
+    cntArea = cv2.contourArea(cnt)
+    if 5000 < cntArea < 10000:
+        cv2.drawContours(img, [cnt], -1, (0, 0, 255), thickness=2)
 
 # draw heart circle
 img = cv2.circle(img, (width//2, height - 100), 60, (128, 128, 128), -1)
 cv2.imshow('kuri', img)
 
 
-def changeEmotion(emotion):
-    color = emotions[emotion]
-    cv2.circle(img, (width // 2, height - 100), 60, color, -1)
-    cv2.imshow('kuri', img)
-
-
-emotion = input("Pick emotion (positive, negative, neutral): ")
-changeEmotion(emotion)
-
-cv2.waitKey(0)
-
-
-# This is the structure I envisioned but image won't show
 # def changeEmotion(emotion):
 #     color = emotions[emotion]
 #     cv2.circle(img, (width // 2, height - 100), 60, color, -1)
 #     cv2.imshow('kuri', img)
-#
-#
-# if __name__ == "__main__":
-#     cv2.imshow('kuri', img)
-#     while True:
-#         emotion = input("Pick emotion (positive, negative, neutral): ")
-#         changeEmotion(emotion)
+
+
+# emotion = input("Pick emotion (positive, negative, neutral): ")
+# changeEmotion(emotion)
+
+cv2.waitKey(0)
