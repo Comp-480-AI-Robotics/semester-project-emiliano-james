646cdf9393fe6eada94a9cbdc548b21ad0395150
Lily <lily.irvin@gmail.com>
2020-05-03 14:21:17 -0500

    expanding heart

--- TextBlobtest.py
+++ TextBlobtest.py
@@ -1,7 +1,7 @@
 from textblob import TextBlob
 import nltk
-# import ssl
-#
+import ssl
+
 # try:
 #     _create_unverified_https_context = ssl._create_unverified_context
 # except AttributeError:
@@ -9,7 +9,7 @@ import nltk
 # else:
 #     ssl._create_default_https_context = _create_unverified_https_context
 #
-# Need to install brown, punkt, wordnet, averaged_perceptron_tagger, conll2000, movie_reviews
+# # Need to install brown, punkt, wordnet, averaged_perceptron_tagger, conll2000, movie_reviews
 # nltk.download()
 
 while True:
--- kuriEmotions.py
+++ kuriEmotions.py
@@ -1,7 +1,7 @@
 import cv2
-import numpy as np
+import time
 
-emotions = {'negative': (0, 0, 255), 'positive': (0, 255, 255), 'neutral': (128, 128, 128)}
+emotionColors = {'negative': (0, 0, 255), 'positive': (0, 255, 255), 'neutral': (128, 128, 128)}
 img = cv2.imread('./kuri.jpg')
 
 # resize image
@@ -16,25 +16,69 @@ greyImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 res, thresh = cv2.threshold(greyImg, 120, 255, 0)
 newImg, contours, hier = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 eyes = []
+left_center, left_axes, right_center, right_axes, theta = None, None, None, None, None
 for cnt in contours:
     cntArea = cv2.contourArea(cnt)
     if 5000 < cntArea < 10000:
         eyes = cnt
         ellipse = cv2.fitEllipse(eyes)
-        img = cv2.ellipse(img, ellipse, (0, 0, 0 ), thickness=-1)
+        img = cv2.ellipse(img, ellipse, (0, 0, 0), thickness=-1)
+        if ellipse[0][0] < width//2:
+            left_center = int(ellipse[0][0]), int(ellipse[0][1])
+            left_axes = int(ellipse[1][0])//2 + 2, int(ellipse[1][1])//2 + 2
+        else:
+            theta = ellipse[2]
+            right_center = int(ellipse[0][0]), int(ellipse[0][1])
+            right_axes = int(ellipse[1][0]) // 2 + 2, int(ellipse[1][1]) // 2 + 2
+
+# Happy Eyes
+img = cv2.ellipse(img, left_center, left_axes, theta, 0, 360, (245, 245, 245), thickness=-1)
+img = cv2.ellipse(img, right_center, right_axes, theta, 0, 360, (245, 245, 245), thickness=-1)
+
+img = cv2.ellipse(img, left_center, left_axes, theta, 0, 180, (0, 0, 0), thickness=20)
+img = cv2.ellipse(img, right_center, right_axes, theta, 0, 180, (0, 0, 0), thickness=20)
 
 # draw heart circle
-img = cv2.circle(img, (width//2, height - 100), 60, (128, 128, 128), -1)
-cv2.imshow('kuri', img)
+
+# For transparency (not working right now)
+# overlay = img.copy()
+# output = img.copy()
 
 
-# def changeEmotion(emotion):
-#     color = emotions[emotion]
-#     cv2.circle(img, (width // 2, height - 100), 60, color, -1)
-#     cv2.imshow('kuri', img)
+def changeCircle(color):
+    a = 40
+    grow = True
+    while True:
+        cv2.circle(img, (width//2, height - 100), a, color, -1)
+        # cv2.addWeighted(overlay, 0.4, output, 0.6, 0, output) # doesn't work correctly
+        time.sleep(0.005)
+        cv2.imshow('kuri', img)
+        cv2.circle(img, (width // 2, height - 100), a, (248, 248, 248), -1)
+        if grow:
+            a += 2
+        else:
+            a -= 2
+        if a >= 80:
+            grow = False
+        if a <= 40:
+            grow = True
+        k = cv2.waitKey(10)
+        if k == 27:
+            break
 
 
-# emotion = input("Pick emotion (positive, negative, neutral): ")
-# changeEmotion(emotion)
+if __name__ == '__main__':
+    cv2.imshow('kuri', img)
+    cv2.waitKey(0)
 
-cv2.waitKey(0)
+    while True:
+        txt = input("Emotion (negative, positive, neutral) (Type 'q' to quit): ")
+
+        if txt.lower() == "q":
+            break
+
+        color = emotionColors[txt.lower()]
+        changeCircle(color)
+
+changeCircle((0, 0, 255))
+cv2.destroyAllWindows()
