b7d6909d0805d8304546688095492003b612b919
EmilianoHuerta <70651959+EmilianoHuerta@users.noreply.github.com>
2022-04-13 16:10:15 -0500

    removal of chat funtionality

--- KuriProgram.py
+++ KuriProgram.py
@@ -6,6 +6,7 @@ Contributors: Emiliano Huerta, James Yang
  ==================================================================="""
 from subprocess import Popen, PIPE
 import io
+import time
 
 from SentimentDetector import SentimentDetector
 from SpeechRecognizer import SpeechRecognizer
@@ -19,31 +20,28 @@ class KuriProgram:
         asks for communication method"""
         self.sd = SentimentDetector()
         self.sr = SpeechRecognizer()
-        self.option = input("Welcome to Kuri! Type 'c' to chat or 's' to speak! ").lower()
+        # self.option = input("Welcome to Kuri! Type 'c' to chat or 's' to speak! ").lower()
         self.proc = proc
 
     def startKuri(self):
         """Starts the program using chat or speech"""
-        if self.option == 'c':
-            self.useChat()
-        elif self.option == 's':
-            self.useSpeech()
+        self.useSpeech()
 
-    def useChat(self):
-        """Displays the Kuri robot and repeatedly takes in user input in text form,
-        while continuously updating the robot's face and heart light"""
-        # Implements a subprocess to run the Kuri robot simultaneously with the user input loop
-        proc_stdin = io.TextIOWrapper(self.proc.stdin, encoding='utf-8', line_buffering=True)
+    # def useChat(self):
+    #     """Displays the Kuri robot and repeatedly takes in user input in text form,
+    #     while continuously updating the robot's face and heart light"""
+    #     # Implements a subprocess to run the Kuri robot simultaneously with the user input loop
+    #     proc_stdin = io.TextIOWrapper(self.proc.stdin, encoding='utf-8', line_buffering=True)
 
-        while True:
-            txt = input("Talk to me! (Type 'q' to quit) ").lower()
-            if txt == 'q':
-                proc_stdin.write('q\n')
-                quit()
-            else:
-                sentiment = self.sd.getSentiment(txt)
-                proc_stdin.write(sentiment + '\n')
-                print("Sentiment: " + sentiment + '\n')
+    #     while True:
+    #         txt = input("Talk to me! (Type 'q' to quit) ").lower()
+    #         if txt == 'q':
+    #             proc_stdin.write('q\n')
+    #             quit()
+    #         else:
+    #             sentiment = self.sd.getSentiment(txt)
+    #             proc_stdin.write(sentiment + '\n')
+    #             print("Sentiment: " + sentiment + '\n')
 
     def useSpeech(self):
         """Displays the Kuri robot and repeatedly takes in user input in speech form,
@@ -52,20 +50,16 @@ class KuriProgram:
         proc_stdin = io.TextIOWrapper(self.proc.stdin, encoding='utf-8', line_buffering=True)
 
         while True:
-            prompt = input("Type 's' to begin recording! (Type 'q' to quit) ").lower()
-            if prompt == 'q':
-                proc_stdin.write('q\n')
-                quit()
-            if prompt == 's':
-                self.sr.kuri_speak("How may I help you?")
-                self.sr.response(self.sr.getSpeech()) 
-                print("Finished processing!")
-                if not self.sr.getSpeech():
-                    print("\nCould you say that again?")
-                else:
-                    sentiment = self.sd.getSentiment(self.sr.getSpeech())
-                    proc_stdin.write(sentiment + '\n')
-                    print("Sentiment: " + sentiment + '\n')
+            time.sleep(1.5)
+            self.sr.kuri_speak("How may I help you?")
+            self.sr.response(self.sr.getSpeech()) 
+            print("Finished processing!")
+            if not self.sr.getSpeech():
+                print("\nCould you say that again?")
+            else:
+                sentiment = self.sd.getSentiment(self.sr.getSpeech())
+                proc_stdin.write(sentiment + '\n')
+                print("Sentiment: " + sentiment + '\n')
 
 
 def RunKuriProgram():
